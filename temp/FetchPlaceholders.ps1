df32910b3fa8c6a715db0737cb61bacf4fbbedec
Manjunath Beli <belibug@outlook.com>
2024-07-08 16:28:50 -0300

    cleanup

--- temp/FetchPlaceholders.ps1
+++
@@ -1,7 +0,0 @@
-$template = Get-Content -Raw 'C:\Localwork\Projects\Mold\sample\psfunc\content.ps1'
-$pattern = '<% MOLD_([^%]+) %>'
-
-
-$matches = [regex]::matches($template, $pattern)
-$placeholders = $matches | ForEach-Object { $_.Groups[1].Value }
-$placeholders
